4d1e108e7c797472682b410741fbe62e288bdb43
Maciej Rzadzinski <volspree@gmail.com>
2022-06-07 21:42:01 +0200

    Complete function handling player's input

--- js/js.js
+++ js/js.js
@@ -1,8 +1,3 @@
-// Get random number between 0-8
-function getRandomInt() {
-    return Math.floor(Math.random() * 9);
-}
-
 // Randomly generate rock, paper or scissors for computer
 function computerPlay() {
     let randomInt = getRandomInt();
@@ -13,4 +8,29 @@ function computerPlay() {
     } else {
         return "Scissors";
     }
+}
+
+// Get random number between 0-8
+function getRandomInt() {
+    return Math.floor(Math.random() * 9);
+}
+
+// Get player's input, case insensitive
+function playerSelection() {
+    let input = prompt('Rock, paper or scissors?');
+    input = input.toLowerCase();
+    console.log(input);
+    if (input === 'rock') {
+        return 'Rock';
+    } else if (input === 'paper') {
+        return 'Paper';
+    } else if (input === 'scissors') {
+        return 'Scissors';
+    }
+}
+
+
+
+function playRound(playerSelection(), computerPlay()) {
+    
 }
